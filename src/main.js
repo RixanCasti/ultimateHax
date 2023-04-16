227796c61506b1ba17f50df71efc95219480fbdd
RixCasti <rixancasti@gmail.com>
2023-04-16 12:16:57 -0500

    Better code

--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ app.on('ready', createWindow);
 app.on('activate', createWindow);
 app.on('window-all-closed', app.quit);
 
-app.commandLine.appendSwitch('disable-http-cache');
-app.commandLine.appendSwitch('enable-pre-commit-input');
-app.commandLine.appendSwitch('disable-frame-rate-limit');
-app.commandLine.appendSwitch('disable-arc-cpu-restriction');
-app.commandLine.appendSwitch('enable-accelerated-2d-canvas');
+const commandLineFlags = [
+    'disable-http-cache',
+    'enable-pre-commit-input',
+    'disable-frame-rate-limit',
+    'disable-arc-cpu-restriction',
+    'enable-accelerated-2d-canvas'
+];
+
+commandsLineFlags.forEach(flag => app.commandLine.appendSwitch(flag));
 
 function createWindow () {
     const win = new BrowserWindow({
